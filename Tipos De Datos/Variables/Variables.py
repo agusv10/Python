b9c35f6538c616dbeca3c74ce808b1df9b22d1a8
Agustín Vedia <agustindonatovedia@gmail.com>
2025-07-05 23:53:00 -0300

    Versión Inicial

---
+++ Tipos De Datos/Variables/Variables.py
@@ -0,0 +1,5 @@
+nombre = "Agustín"
+bienvenida = " Hola " + nombre + " ¿Cómo estás?" + " ¿Ya tienes novia?"
+python = "Python"
+respuesta = " Aún no " + python + " pero pronto la tendré"
+print (bienvenida + respuesta)
---
+++ Tipos De Datos/tipos-de-datos.py
@@ -0,0 +1,16 @@
+"string"
+'string'
+
+"""tus datos son:
+      nombre: Agustín
+      apellido: Vedia"""
+
+'''string'''
+
+40
+40.2
+
+"booleano"
+
+False
+True
